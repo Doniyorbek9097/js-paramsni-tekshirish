7da65876aec7ab6c3303c77aba4d95f7a7bd27fe
Doniyorbek9097 <doniyorbek3322@mail.ru>
2021-08-05 14:03:12 +0100

    js-player

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+
+import "./style.scss";
+
+
 let audioList = ["http://k007.kiwi6.com/hotlink/i439fjfmqh/Shake_-JaeJoe_Ft._AceahSpades_and_Thrillz.mp3",
 "http://k007.kiwi6.com/hotlink/i8321m92n2/a_va_p_ter.mp3","http://k007.kiwi6.com/hotlink/sce3rezufz/Shadow_Puppets.mp3","http://k007.kiwi6.com/hotlink/3yr5g29xpq/Post-Nuclear_Apocalypse_Blues.mp3"];
 let audio = new Audio(audioList[0])
 let corrent = 0;
+
+
+
+function getName(){
+    
+    let splitName =  audioList[corrent].split("/");
+      let musicName = splitName[splitName.length-1];
+      audioName.innerHTML = musicName;
+}
+
 play.onclick = function(){
     audio.play()
     pauza.style.display="block";
     play.style.display="none";
-    
+    getName();
 }
 
 pauza.onclick = function(){
     audio.pause();
     pauza.style.display="none";
     play.style.display="block";
+    getName();
 }
 
 next.onclick = function(){
@@ -20,7 +35,8 @@ next.onclick = function(){
     if(audioList[corrent] !== undefined){
         audio.pause();
         audio = new Audio(audioList[corrent]);
-        audio.play()
+        audio.play();
+        getName();
     }
 
 }
@@ -32,6 +48,7 @@ prev.onclick = function(){
         audio = new Audio(audioList[corrent]);
         console.log(audio);
         audio.play();
+        getName();
     }
 
 }
